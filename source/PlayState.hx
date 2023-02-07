f21150743d7153ab06111485f753d31bcfbe7791
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-02-07 18:19:51 +1000

    add basic racket

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,14 +1,35 @@
 package;
 
 import flixel.FlxState;
+import flixel.util.FlxDirection;
 
 class PlayState extends FlxState {
 
+	var player:Racket;
+
 	override public function create() {
 		super.create();
+
+		player = new Racket(100, 15, FlxDirection.RIGHT);
+		player.screenCenter();
+
+		add(player);
 	}
 
 	override public function update(elapsed:Float) {
+		moveRacketByKeyboard(player);
 		super.update(elapsed);
+	}
+
+	function moveRacketByKeyboard(racket:Racket) {
+		racket.velocity.set(0, 0);
+
+		var pressed = Flixel.keys.pressed;
+		if (pressed.UP) {
+			racket.velocity.setPolarDegrees(200, -90);
+		}
+		else if (pressed.DOWN) {
+			racket.velocity.setPolarDegrees(200, 90);
+		}
 	}
 }
---
+++ source/Racket.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.util.FlxDirection;
+
+class Racket extends FlxSprite {
+
+	public var direction:FlxDirection;
+
+	public function new(width:Int, height:Int, direction:FlxDirection = FlxDirection.UP) {
+		super();
+
+		this.direction = direction;
+		makeGraphic(width, height, FlxColor.WHITE);
+		switch (direction) {
+			case UP:
+				angle = 0;
+			case RIGHT:
+				angle = 90;
+			case DOWN:
+				angle = 180;
+			case LEFT:
+				angle = 270;
+		}
+	}
+}
