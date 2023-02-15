e7b073805701c65904d172cdc9c1780a1e2612df
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-02-15 20:13:36 +1000

    cleanup

--- source/ai/SimpleAI.hx
+++ source/ai/SimpleAI.hx
@@ -5,30 +5,10 @@ import flixel.math.FlxRect;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 
-abstract Bounds(Array<Float>) from Array<Float> {
-	public var min(get, set):Float;
-	public var max(get, set):Float;
-
-	inline function get_min()
-		return this[0];
-
-	inline function set_min(v)
-		return this[0] = v;
-
-	inline function get_max()
-		return this[1];
-
-	inline function set_max(v)
-		return this[1] = v;
-}
-
 class SimpleAI extends RacketController {
 
 	var tmprect1 = new FlxRect();
 	var tmprect2 = new FlxRect();
-
-	var boundsX:Bounds;
-	var boundsY:Bounds;
 
 	var targetX:Float;
 	var targetY:Float;
@@ -41,9 +21,6 @@ class SimpleAI extends RacketController {
 
 	public function new(racket:Racket) {
 		super(racket);
-
-		boundsX = [0, Flixel.height];
-		boundsY = [0, Flixel.width];
 
 		targetX = Flixel.width * 0.5;
 		targetY = Flixel.height * 0.5;
@@ -96,53 +73,4 @@ class SimpleAI extends RacketController {
 
 		currentTimer += dt;
 	}
-
-	// function updateOld(dt) {
-	// 	var ball = getBall();
-	// 	if (ball == null || FlxMath.equal(ball.velocity.lengthSquared, 0, 0.1))
-	// 		return;
-	// 	// check if it is time to rethink racket position
-	// 	if (currentTimer >= timeToRethink) {
-	// 		currentTimer = 0;
-	// 		var factor = invLerp(Flixel.width, 0, ball.x);
-	// 		var upperBound = FlxMath.lerp(0.1, 0.3, factor);
-	// 		timeToRethink = Flixel.random.float(0.1, upperBound);
-	// 		trace('upperBound: $upperBound');
-	// 	}
-	// 	// ten times in sec recalc new ball pos
-	// 	// 2 times in sec
-	// 	// get ball boundaries (screen or world) BB
-	// 	// get racket boundaries (screen or world) RB
-	// 	// calc target racket position RT:
-	// 	//   RT must overlap BB
-	// 	var ballBounds = ball.getHitbox(tmprect1);
-	// 	var racketBounds = racket.getHitbox(tmprect2);
-	// 	// calculate racket target position
-	// 	switch (racket.direction) {
-	// 		case LEFT, RIGHT:
-	// 			// Getting bounds of available space to maneuver
-	// 			// to hit the ball.
-	// 			var top = ballBounds.top + 1 - racketBounds.height;
-	// 			var bottom = ballBounds.bottom - 1 + racketBounds.height;
-	// 			var topY = top;
-	// 			var bottomY = bottom - racketBounds.height;
-	// 			targetY = Flixel.random.float(topY, bottomY);
-	// 		case UP, DOWN:
-	// 			// TODO
-	// 			0;
-	// 	}
-	// 	// Racket target pos updates constantly,
-	// 	// but applies on `timeToRethink` interval!
-	// 	if (currentTimer == 0) {
-	// 		trace('rethink');
-	// 		if (tween != null)
-	// 			tween.cancel();
-	// 		// trace(targetY, racket.y, racketBounds.y);
-	// 		var path = Math.abs(targetY - racketBounds.y);
-	// 		var duration = path / Pong.defaults.racketSpeed;
-	// 		// trace('time to traverse $path px is $duration');
-	// 		tween = FlxTween.tween(racket, {y: targetY}, duration, {ease: FlxEase.linear});
-	// 	}
-	// 	currentTimer += dt;
-	// }
 }
