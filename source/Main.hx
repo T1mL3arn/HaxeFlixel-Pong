a25c27d8bdbec67d4c19a828346a50c801354cf3
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-02-15 20:19:51 +1000

    actually run a room with ai players

--- source/Main.hx
+++ source/Main.hx
@@ -1,15 +1,29 @@
 package;
 
+import ai.SimpleAI;
 import flixel.FlxGame;
+import flixel.util.FlxDirection;
 import openfl.display.Sprite;
 
 class Main extends Sprite {
 
 	public function new() {
 		super();
-		// addChild(new FlxGame(0, 0, TrainingRoom));
+
 		addChild(new FlxGame(0, 0));
-		Flixel.switchState(new TwoPlayersRoom());
+
+		// Flixel.switchState(new TwoPlayersRoom(null, {
+		// 	position: FlxDirection.RIGHT,
+		// 	getController: racket -> new SimpleAI(racket)
+		// }));
+
+		Flixel.switchState(new TwoPlayersRoom({
+			position: FlxDirection.LEFT,
+			getController: racket -> new SimpleAI(racket)
+		}, {
+			position: FlxDirection.RIGHT,
+			getController: racket -> new SimpleAI(racket)
+		}));
 	}
 }
 
