8a86437335f12bddd9f5325dac58401348946cf4
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-02-16 20:25:04 +1000

    add inverse lerp

--- source/Utils.hx
+++ source/Utils.hx
@@ -3,11 +3,15 @@ package;
 using Reflect;
 
 /**
-	Merge all fields from `source` into `target`.
+	Merges all fields from `source` into `target`.
 	This method is only guaranteed to work on anonymous structures.
 **/
 function merge<A, B, AB:A & B>(target:A, source:B):AB {
 	for (fieldName in source.fields())
 		target.setField(fieldName, source.field(fieldName));
 	return cast target;
+}
+
+inline function invLerp(a:Float, b:Float, v:Float):Float {
+	return (v - a) / (b - a);
 }
