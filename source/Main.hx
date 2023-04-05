1b622d2bfec2cc3e0016d1d006396a9a5017243a
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-04-05 20:25:13 +1000

    add main menu state

--- source/Main.hx
+++ source/Main.hx
@@ -1,9 +1,10 @@
 package;
 
+import ai.NotSoSimpleAI;
 import ai.SimpleAI;
 import djFlixel.D;
-import djFlixel.ui.FlxMenu;
 import flixel.util.FlxDirection;
+import menu.MainMenu;
 import openfl.display.Sprite;
 
 class Main extends Sprite {
@@ -15,36 +16,7 @@ class Main extends Sprite {
 
 		addChild(new Pong());
 
-		var menu = new FlxMenu(0, 0, -1);
-
-		menu.PAR.start_button_fire = true;
-
-		menu.createPage('main').add('
-		-| 1 player | link | @1_player
-		-| multiplayer	| link | opts
-		-| exit			| link | exit
-		');
-
-		menu.createPage('1_player').add('
-		-| training room | link | training_room
-		-| vs AI | link | ai
-		-| go back | link | @back
-		');
-
-		// disabling menu items tweening
-		menu.STP.focus_anim = null;
-		menu.STP.vt_IN = menu.STP.vt_OUT = '0:0|0:0';
-		menu.STP.vt_in_ease = null;
-		// setting up menu items text
-		menu.STP.align = 'center';
-		menu.STP.item.text = {
-			s: 30,
-			a: 'center'
-		};
-
-		menu.goto('main');
-
-		Flixel.state.add(menu);
+		Flixel.switchState(new MainMenu());
 
 		return;
 
---
+++ source/menu/MainMenu.hx
@@ -0,0 +1,75 @@
+package menu;
+
+import RacketController.KeyboardMovementController;
+import djFlixel.ui.FlxMenu;
+import flixel.FlxState;
+import flixel.util.FlxColor;
+import flixel.util.FlxDirection;
+
+class MainMenu extends FlxState {
+
+	static final TRAINING_ROOM_MENU_ID = 'training_room';
+	static final SELF_ROOM_MENU_ID = 'self';
+
+	public function new() {
+		super();
+	}
+
+	override function create() {
+
+		var menu = new FlxMenu(0, Flixel.game.height / 2 - 100, -1);
+
+		menu.PAR.start_button_fire = true;
+
+		menu.createPage('main').add('
+		-| 1 player | link | @1_player
+		-| multiplayer	| link | opts
+		-| exit			| link | exit
+		');
+
+		menu.createPage('1_player')
+			.add('
+		-| training room | link | ${TRAINING_ROOM_MENU_ID}
+		-| vs self | link | ${SELF_ROOM_MENU_ID}
+		-| vs AI | link | ai
+		-| go back | link | @back
+		');
+
+		// disabling menu items tweening
+		menu.STP.focus_anim = null;
+		menu.STP.vt_IN = menu.STP.vt_OUT = '0:0|0:0';
+		menu.STP.vt_in_ease = null;
+		// setting up menu items text
+		menu.STP.align = 'center';
+		menu.STP.item.text = {
+			s: 30,
+			a: 'center'
+		};
+
+		menu.goto('main');
+		menu.onMenuEvent = (e, id) -> {
+			switch ([e, id]) {
+				case [MenuEvent.it_fire, TRAINING_ROOM_MENU_ID]:
+					Flixel.switchState(new TrainingRoom());
+
+				case [MenuEvent.it_fire, SELF_ROOM_MENU_ID]:
+					Flixel.switchState(new TwoPlayersRoom({
+						position: FlxDirection.LEFT,
+						name: 'you',
+						color: FlxColor.WHITE,
+						getController: racket -> new KeyboardMovementController(racket),
+					}, {
+						position: FlxDirection.RIGHT,
+						name: 'also you',
+						color: FlxColor.WHITE,
+						getController: racket -> new KeyboardMovementController(racket),
+					}));
+
+				default:
+					0;
+			}
+		}
+
+		add(menu);
+	}
+}
