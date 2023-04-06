254b0ca4321320c770404e85ddd0b625c60a7550
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-04-06 22:55:38 +1000

    separate menu styling from main menu

--- source/menu/MainMenu.hx
+++ source/menu/MainMenu.hx
@@ -35,16 +35,7 @@ class MainMenu extends FlxState {
 		-| go back | link | @back
 		');
 
-		// disabling menu items tweening
-		menu.STP.focus_anim = null;
-		menu.STP.vt_IN = menu.STP.vt_OUT = '0:0|0:0';
-		menu.STP.vt_in_ease = null;
-		// setting up menu items text
-		menu.STP.align = 'center';
-		menu.STP.item.text = {
-			s: 30,
-			a: 'center'
-		};
+		MenuStyle.setDefaultStyle(menu);
 
 		menu.goto('main');
 		menu.onMenuEvent = (e, id) -> {
---
+++ source/menu/MenuStyle.hx
@@ -0,0 +1,19 @@
+package menu;
+
+import djFlixel.ui.FlxMenu;
+
+class MenuStyle {
+
+	public static function setDefaultStyle(menu:FlxMenu) {
+		// disabling menu items tweening
+		menu.STP.focus_anim = null;
+		menu.STP.vt_IN = menu.STP.vt_OUT = '0:0|0:0';
+		menu.STP.vt_in_ease = null;
+		// setting up menu items text
+		menu.STP.align = 'center';
+		menu.STP.item.text = {
+			s: 30,
+			a: 'center'
+		};
+	}
+}
