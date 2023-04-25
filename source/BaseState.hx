1d11e27c26c0c3a7ea98f93d298e33003209e050
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-04-25 18:03:16 +1000

    fix closing pause menu

--- source/BaseState.hx
+++ source/BaseState.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxState;
+import flixel.input.keyboard.FlxKey;
 import menu.PauseMenu;
 
 class BaseState extends FlxState {
@@ -9,6 +10,8 @@ class BaseState extends FlxState {
 		super.update(elapsed);
 
 		if (Flixel.keys.anyJustPressed([ESCAPE, P])) {
+			@:privateAccess Flixel.keys._keyListMap.get(FlxKey.ESCAPE).reset();
+			@:privateAccess Flixel.keys._keyListMap.get(FlxKey.P).reset();
 			openSubState(new PauseMenu());
 		}
 	}
