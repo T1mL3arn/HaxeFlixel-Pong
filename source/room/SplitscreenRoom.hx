a8c9c03623355174d020bbd3c437ebd87054d160
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-04-28 18:41:52 +1000

    refactoring split screen help info

--- source/room/SplitscreenRoom.hx
+++ source/room/SplitscreenRoom.hx
@@ -1,6 +1,9 @@
 package room;
 
 import flixel.FlxSubState;
+import flixel.effects.FlxFlicker;
+import flixel.group.FlxGroup;
+import flixel.input.keyboard.FlxKey;
 import flixel.text.FlxText;
 
 class SplitscreenRoom extends TwoPlayersRoom {
@@ -22,8 +25,9 @@ class SplitscreenRoom extends TwoPlayersRoom {
 class SplitscreenRoomGuide extends FlxSubState {
 
 	var room:SplitscreenRoom;
-	var leftIsReady:Bool = false;
-	var rightIsReady:Bool = false;
+
+	var left:PlayerGuideUI;
+	var right:PlayerGuideUI;
 
 	public function new(room) {
 		super();
@@ -33,38 +37,65 @@ class SplitscreenRoomGuide extends FlxSubState {
 	override function create() {
 		super.create();
 
-		add(buildGuideUI('W and S', Flixel.width * 0.25));
-		add(buildGuideUI('UP and DOWN', Flixel.width * 0.75));
-	}
-
-	function buildGuideUI(labelText:String, ?alignLineX:Float):FlxText {
-
-		var w = Flixel.width * 0.5 * 0.8;
-		var y = Flixel.height * 0.6;
-		var x = alignLineX - w * 0.5;
-
-		labelText = 'use ${labelText} keys\nto move your paddle';
-
-		var label = new FlxText(x, y, w, labelText, 18);
-		label.width = w;
-		label.alignment = CENTER;
-
-		return label;
+		add(left = new PlayerGuideUI('W and S', [W, S], Flixel.width * 0.25));
+		add(right = new PlayerGuideUI('UP and DOWN', [UP, DOWN], Flixel.width * 0.75));
 	}
 
 	override function update(elapsed:Float) {
 		super.update(elapsed);
 
-		if (Flixel.keys.anyJustPressed([W, S])) {
-			leftIsReady = true;
-		}
-
-		if (Flixel.keys.anyJustPressed([UP, DOWN])) {
-			rightIsReady = true;
-		}
-
-		if (leftIsReady && rightIsReady) {
+		if (left.playerIsReady && right.playerIsReady) {
 			close();
 		}
+	}
+}
+
+class PlayerGuideUI extends FlxGroup {
+
+	public var playerIsReady:Bool = false;
+
+	var keys:Array<FlxKey>;
+	var flicker:FlxFlicker;
+
+	public function new(labelText:String, keys:Array<FlxKey>, xAlignCoord:Float) {
+		super();
+
+		this.keys = keys;
+
+		var w = Flixel.width * 0.5 * 0.8;
+		var y = Flixel.height * 0.6;
+		var x = xAlignCoord - w * 0.5;
+
+		labelText = 'press ${labelText}\nto move your paddle';
+
+		var infoLabel = new FlxText(x, y, w, labelText, 18);
+		infoLabel.width = w;
+		infoLabel.alignment = CENTER;
+
+		add(infoLabel);
+
+		var waitLabel = new FlxText(x, Flixel.height * 0.8, w, 'waiting player', 16);
+		waitLabel.width = w;
+		waitLabel.alignment = CENTER;
+
+		add(waitLabel);
+
+		flicker = FlxFlicker.flicker(waitLabel, 0, 0.5);
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (Flixel.keys.anyJustPressed(keys)) {
+			playerIsReady = true;
+			if (flicker != null) {
+				flicker.stop();
+				flicker = null;
+			}
+		}
+	}
+
+	override function destroy() {
+		super.destroy();
 	}
 }
