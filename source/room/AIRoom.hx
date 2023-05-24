a2e8a5f939c43d4c8ae8ee64d9537a480bb131bb
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-05-25 01:00:36 +1000

    speed up AI

--- source/room/AIRoom.hx
+++ source/room/AIRoom.hx
@@ -6,5 +6,7 @@ abstract AIRoom(TwoPlayersRoom) to TwoPlayersRoom {
 
 	public function new(left:String = 'easy', right:String = 'easy') {
 		this = new TwoPlayersRoom(setAIPlayer({position: LEFT}, left), setAIPlayer({position: RIGHT}, right));
+		Pong.params.scoreToWin = 2;
+		Pong.params.ballSpeed *= 2;
 	}
 }
