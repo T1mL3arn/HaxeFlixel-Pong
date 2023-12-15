ad34a9b2ba47e571b2e7e47800544e55b9d05372
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-12-15 16:48:01 +1000

    del old comments

--- source/shader/CrtShader.hx
+++ source/shader/CrtShader.hx
@@ -13,30 +13,11 @@ class CrtShader extends FlxShader {
 	public function new() {
 		super();
 
-		/* 
-			Current way of reading shader content didn't work for me, I tried to fix it. 
-
-			(trying to use Codespaces to push suggestions)
-		 */
-
-		// NOTE it is parsed by Haxe Macro from shader code
 		this.iTime.value = [0.0];
-
-		// shader distortion shit
-		// https://discord.com/channels/162395145352904705/165234904815239168/1044303305108639834
-		// https://dixonary.co.uk/blog/shadertoy#a-couple-of-gotchas
-
-		/*
-			let smoothstep = (a,b,v) => {
-			let t = (v-a)/(b-a);
-			t = Math.max(0.0, t);
-			t = Math.min(t, 1.0);
-			return t*t*(3.0 - 2.0*t);
-			}
-		 */
 	}
 
 	public function update(time:Float) {
+
 		this.iTime.value[0] += time;
 	}
 }
