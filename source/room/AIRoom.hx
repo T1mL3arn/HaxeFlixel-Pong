69a66853b48613313bc97e817b77ac6ce646875f
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-12-19 00:24:27 +1000

    add AI playing pong as main menu background

--- source/room/AIRoom.hx
+++ source/room/AIRoom.hx
@@ -1,18 +1,57 @@
 package room;
 
 import ai.AIFactory.setAIPlayer;
+import menu.CongratScreen.CongratScreenType;
+import menu.CongratScreen;
 
-abstract AIRoom(TwoPlayersRoom) to TwoPlayersRoom {
+class AIRoom extends TwoPlayersRoom {
 
-	public inline function new(left:String = 'easy', right:String = 'easy') {
-		this = new TwoPlayersRoom(setAIPlayer({position: LEFT}, left), setAIPlayer({position: RIGHT}, right));
+	var endless:Bool;
 
-		var oldCloseCallback = this.closeCallback;
-		this.closeCallback = () -> {
-			if (oldCloseCallback != null)
-				oldCloseCallback();
-			// restore default game params;
-			Pong.params = Reflect.copy(Pong.defaultParams);
+	public function new(left:String = null, right:String = null, endless:Bool = false) {
+
+		left = left ?? getRandomAi();
+		right = right ?? getRandomAi();
+
+		super(setAIPlayer({position: LEFT}, left), setAIPlayer({position: RIGHT}, right));
+
+		this.endless = endless;
+	}
+
+	inline function getRandomAi() {
+		return Flixel.random.getObject(['easy', 'medium', 'hard']);
+	}
+
+	override function create() {
+		super.create();
+
+		Flixel.watch.addQuick('left', leftOptions.name);
+		Flixel.watch.addQuick('right', rightOptions.name);
+
+		setGameParams();
+		// re-init speedup to use new Pong.params
+		ballSpeedup.init();
+	}
+
+	function setGameParams() {
+		Pong.resetParams();
+		Pong.params.scoreToWin = 3;
+		Pong.params.ballSpeed *= 1.15;
+	}
+
+	override function showCongratScreen(player:Player, screenType:CongratScreenType) {
+		if (endless) {
+			// re-enable AI players
+			for (player in players) {
+				player.active = true;
+				player.score = 0;
+			}
+			setGameParams();
+			ballSpeedup.init();
+			return;
 		}
+
+		var playAgainAction = _ -> Flixel.switchState(new AIRoom(getRandomAi(), getRandomAi()));
+		openSubState(new CongratScreen(playAgainAction).setWinner(player.name, screenType));
 	}
 }
