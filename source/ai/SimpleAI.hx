145db3baa82c1c75a32b4fcd08158927f385737a
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-12-28 20:24:27 +1000

    simple AI wip

--- source/ai/SimpleAI.hx
+++ source/ai/SimpleAI.hx
@@ -1,8 +1,12 @@
 package ai;
 
+import flixel.math.FlxMath;
 import flixel.math.FlxRect;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
+import flixel.tweens.misc.VarTween;
+import flixel.util.FlxTimer;
+import ai.BaseAI.BaseAI2;
 
 /**
 	Simple AI does not mean dumb AI. 
@@ -11,21 +15,35 @@ import flixel.tweens.FlxTween;
 		- Try to match racket center Y coord with it.
 	NOTE: This AI is not good when a ball comes at sharp adges.
 **/
-class SimpleAI extends RacketController {
-
-	public var name:String;
-
-	var timeToThink:Float = 0.1;
-	var timer:Float;
+class SimpleAI345 extends BaseAI2 {
 
 	var tmprect1:FlxRect = FlxRect.get();
 	var tmprect2:FlxRect = FlxRect.get();
-	var tween:FlxTween;
+
+	var tweenProps = {y: 0.0};
+	var tweenOptions = {
+		ease: FlxEase.linear,
+		onComplete: null,
+		startDelay: 0.0,
+	};
 
 	public function new(racket:Racket, ?name:String) {
-		super(racket);
+		super(racket, name ?? 'simple AI');
 
-		this.name = name ?? 'simple AI';
+		timeToThink = 0.075;
+		tweenOptions.onComplete = _ -> {
+			tweenOptions.startDelay = 0;
+			followBall(positionVariance);
+		};
+
+		var rnd = Flixel.random;
+		// position variance timer
+		new FlxTimer().start(rnd.float(1.5, 2.5), _ -> positionVariance = rnd.int(-95, 95) * 0.01, 0);
+		// tween delay timer
+		new FlxTimer().start(rnd.float(0.5, 1.5), _ -> {
+			tweenOptions.startDelay = rnd.float() < 0.75 ? 0.0 : rnd.float(timeToThink * 0.3, timeToThink);
+			followBall(positionVariance);
+		}, 0);
 	}
 
 	override function destroy() {
@@ -51,25 +69,41 @@ class SimpleAI extends RacketController {
 		}
 
 		if (timer == 0) {
-			var ballBounds = ball.getHitbox(tmprect1);
-			var racketBounds = racket.getHitbox(tmprect2);
-
-			switch (racket.position) {
-				case LEFT, RIGHT:
-					var targetCenterY = (ballBounds.y + ballBounds.bottom) / 2;
-					var targetRacketY = targetCenterY - racketBounds.height / 2;
-
-					if (tween != null)
-						tween.cancel();
-
-					var path = Math.abs(targetRacketY - racketBounds.y);
-					var duration = path / Pong.params.racketSpeed;
-					tween = GAME.aiTweens.tween(racket, {y: targetRacketY}, duration, {ease: FlxEase.linear});
-				case UP, DOWN:
-					throw "Implement it later";
-			}
+			followBall(positionVariance);
 		}
 
 		timer += dt;
+	}
+
+	/**
+		@param variance value in range [-1.0, 1.0]
+	**/
+	function followBall(variance:Float = 0.0) {
+		// instruct the racket to follow the ball
+		var ball = GAME.room.ball;
+
+		switch (racket.position) {
+			case LEFT, RIGHT:
+				// var min = ball.y + 1 - racket.height;
+				// var max = ball.y + ball.height - 1;
+				// HZ (hitzone) = max - min
+				var HZ = racket.height + ball.height - 2;
+
+				variance = (variance + 1.0) * 0.5;
+				variance = FlxMath.bound(variance, 0.0, 1.0);
+				var targetRacketY = FlxMath.lerp(ball.y + 1 - racket.height, ball.y + ball.height - 1, variance);
+
+				var path = Math.abs(targetRacketY - racket.y);
+				var duration = path / Pong.params.racketSpeed;
+				tweenProps.y = targetRacketY;
+
+				if (tween != null)
+					tween.cancel();
+
+				tween = GAME.aiTweens.tween(racket, tweenProps, duration, tweenOptions);
+
+			case _:
+				throw "Implement it later";
+		}
 	}
 }
